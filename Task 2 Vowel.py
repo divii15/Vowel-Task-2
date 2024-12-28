77a9cd45531b169087bd661ef9836e8b1629d88a
divii15 <divyadaksha143@gmail.com>
2024-12-28 16:32:54 +0530

    My new task(2)(1) 28/12

---
+++ Task 2 Vowel.py
@@ -0,0 +1,5 @@
+let=input("Enter a letter:")
+if(let=='A' or let=='E' or let=='I' or let=='O' or let=='U' or let=='a' or let=='e' or let=='i' or let=='o' or let=='u'):
+      print("It is a vowel letter")
+else :
+    print("It is a consonant letter")
