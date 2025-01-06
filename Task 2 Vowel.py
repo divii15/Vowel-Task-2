51cf4d290ab8c138c7661dcda664729311cd7c8a
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:10:35 +0530

    My new task(2)(1)

--- Task 2 Vowel.py
+++ Task 2 Vowel.py
@@ -1,3 +1,7 @@
+#TASK 2
+#1)Enter the letter.find is vowel or not?
+
+
 let=input("Enter a letter:")
 if(let=='A' or let=='E' or let=='I' or let=='O' or let=='U' or let=='a' or let=='e' or let=='i' or let=='o' or let=='u'):
       print("It is a vowel letter")
